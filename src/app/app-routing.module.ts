054bb190b398b269294a457fe9bc8d0b31f60955
Santichai <santichai2959@gmail.com>
2022-08-06 23:51:15 +0700

    Update Version 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './page/home/home.component';
-import { ResumeComponent } from './page/resume/resume.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'profile', pathMatch: 'full' },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,32 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { HeaderComponent } from './components/header/header.component';
-import { AboutmeComponent } from './content/aboutme/aboutme.component';
-import { SkillComponent } from './content/skill/skill.component';
-import { ExpComponent } from './content/exp/exp.component';
-import { HomeComponent } from './page/home/home.component';
-import { ResumeComponent } from './page/resume/resume.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    FooterComponent,
-    HeaderComponent,
-    AboutmeComponent,
-    SkillComponent,
-    ExpComponent,
-    HomeComponent,
-    ResumeComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
